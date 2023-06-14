78ef712d726d80742bb05e49421bb819b1c76a06
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-06-14 15:44:49 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "next";
+import { columns } from "@/components/user/columns";
+import DataTable from "@/components/user/DataTable";
+import { getUsers } from "@/services/userService";
+import LogoutButton from "@/components/user/LogoutButton";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+export default async function Home() {
+  const users = await getUsers();
+
+  return (
+    <div className="flex-col flex-1 hidden h-full p-8 space-y-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">
+            Welcome to LIKELION!
+          </h2>
+          <p className="text-muted-foreground">
+            Here&apos;s a list of users who registered courses!
+          </p>
+        </div>
+        <div>
+          <LogoutButton />
+        </div>
+      </div>
+
+      {!users ? (
+        <div>No users found!</div>
+      ) : (
+        <DataTable data={users} columns={columns} />
+      )}
+    </div>
+  );
+}
